fbe9e71d1f7397f6c542bbf4d3666ad7ec0f5ed0
FTMFR <fatemefarajzade018@gmail.com>
2023-09-29 21:40:51 +0330

    first initial

---
+++ frontend/src/context/AuthProvider.js
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+const Authcontext = createContext({});
+
+
+
+export const AuthProvider = (props) => {
+    const [auth, setAuth] = useState({});
+
+    return (
+        <Authcontext.Provider
+            value={{ auth, setAuth }}
+        >
+            {props.children}
+        </Authcontext.Provider>
+    )
+};
+
+export default Authcontext;
